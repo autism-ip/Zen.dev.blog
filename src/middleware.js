72caf80512933a3a7e59233ff59dbbb76a229612
Zen <142089698+autism-ip@users.noreply.github.com>
2025-12-06 23:19:25 +0800

    modified:   src/middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -7,7 +7,7 @@ export function middleware(request, event) {
   async function sendAnalytics() {
     const URL =
       process.env.NODE_ENV === 'production'
-        ? 'https://zenhungyep.com/api/increment-views'
+        ? '${process.env.NEXT_PUBLIC_BASE_URL}/api/increment-views'
         : 'http://localhost:3000/api/increment-views'
 
     try {
